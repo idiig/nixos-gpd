f0199c0feae9feefe86ac50fc3d27adce3603a19
idiig <avionplat@hotmail.com>
2024-03-19 22:24:41 +0900

    update xmonad.hs for Java-based GUI applications

--- general/xmonad.hs
+++ general/xmonad.hs
@@ -1,6 +1,10 @@
 import XMonad
 import XMonad.Config.Xfce
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.SetWMName
+
 main = xmonad xfceConfig
        { terminal = "xfce4-terminal"
        , modMask = mod4Mask -- optional: use Win key instead of Alt as MODi key
+       , startupHook = ewmhDesktopsStartup >> setWMName "LG3D" -- for some reason the double greater sign is escaped here due to wiki formatting, replace this with proper greater signs!
        }
