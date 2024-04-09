45ea03a5e4218b0254e34cebb42493a1b08cd349
idiig <avionplat@hotmail.com>
2024-04-09 13:35:48 +0900

    update ssh for remote mac

--- modules/home/app/security-privacy/ssh.nix
+++ modules/home/app/security-privacy/ssh.nix
@@ -13,8 +13,19 @@
   programs.ssh = {
     enable = true;
     extraConfig = ''
-      Host github.com
-        IdentityFile ~/.ssh/id_ed25519
+    Host github.com
+      IdentityFile ~/.ssh/id_ed25519
+      
+    Host macair23
+      HostName 124.38.172.203
+      User idiig
+      UserKnownHostsFile /dev/null
+      PreferredAuthentications publickey
+      StrictHostKeyChecking no
+      PasswordAuthentication no
+      IdentityFile ~/.ssh/idiig_nixos
+      IdentitiesOnly yes
+      LogLevel FATAL
     '';
   };
 
